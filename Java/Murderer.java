9c5edd7cc16c9b93c3cde99df85f1b89c4edf929
Jsosm <140778943+Jsosm@users.noreply.github.com>
2023-07-30 13:44:00 +1200

    Updated Murderer.java
    Should be good to go

--- Java/Murderer.java
+++ Java/Murderer.java
@@ -1,61 +1,32 @@
-/*PLEASE DO NOT EDIT THIS CODE*/
-/*This code was generated using the UMPLE 1.32.1.6535.66c005ced modeling language!*/
-
-
 import java.util.*;
-
-// line 60 "model.ump"
-// line 226 "model.ump"
-public class Murderer extends Game
+public class Murderer
 {
-  //------------------------
-  // DEVELOPER CODE - PROVIDED AS-IS
-  //------------------------
-
-  // line 67 "model.ump"
   private Card weapon ;
-  // line 68 "model.ump"
   private Card estate ;
-  // line 69 "model.ump"
   private Card person ;
-
-  //------------------------
-  // MEMBER VARIABLES
-  //------------------------
 
   //------------------------
   // CONSTRUCTOR
   //------------------------
 
-  public Murderer(int aCurrentPlayerTurn, Board aBoard, List<Card> aAllCards, Murderer aMurderer, Map<Integer,Player> aPlayerMap)
+  public Murderer(Card _weapon, Card _estate, Card _person)
   {
-    super(aCurrentPlayerTurn, aBoard, aAllCards, aMurderer, aPlayerMap);
+    this.weapon = _weapon;
+    this.estate = _estate;
+    this.person = _person;
   }
-
-  //------------------------
-  // INTERFACE
-  //------------------------
-
-  public void delete()
-  {
-    super.delete();
-  }
-
-
   /**
    * Returns if the given cards are the murder ones
    */
-  // line 76 "model.ump"
-  public boolean checkMurderer(Card weapon, Card estate, Card person){
-    return false;
+  public boolean checkMurderer(Card _weapon, Card _estate, Card _person){
+    return this.weapon.equals(_weapon)  && this.estate.equals(_estate) && this.person.equals(_person);
   }
 
 
   /**
    * Formatted display output
    */
-  // line 83 "model.ump"
   public String toString(Card weapon, Card estate, Card person){
-    return "Murder!";
+    return "The Murderer was " + person.getCardName() + "in the" + estate.getCardName() + "with the " + weapon.getCardName();
   }
 }
