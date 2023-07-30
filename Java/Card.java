c95321cda70777225971b4c946db4bb8a09fadff
Jsosm <140778943+Jsosm@users.noreply.github.com>
2023-07-30 14:15:00 +1200

    Update Card.java

--- Java/Card.java
+++ Java/Card.java
@@ -1,10 +1,3 @@
-/*PLEASE DO NOT EDIT THIS CODE*/
-/*This code was generated using the UMPLE 1.32.1.6535.66c005ced modeling language!*/
-
-
-
-// line 144 "model.ump"
-// line 244 "model.ump"
 public class Card
 {
 
